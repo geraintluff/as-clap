08ac43073fdbf33a05317106d826bdda50adcd34
Jairus Tanaka <me@jairus.dev>
2026-01-11 21:17:50 -0800

    fix: allow base asconfig to define any property and support wasi

---
+++ example/transformed/assembly/index.ts
@@ -0,0 +1,38 @@
+export * from "as-clap/clap-entry";
+import * as Clap from "as-clap";
+class MyPlugin extends Clap.Plugin {
+  constructor(host: Clap.Host) {
+    super(host);
+  }
+  pluginInit(): bool {
+    console.log(`Plugin initialised!  Module path is ${Clap.modulePath}`);
+    return true;
+  }
+  pluginProcess(process: Clap.Process): i32 {
+    let audioIn = process.audioInputs[0];
+    let audioOut = process.audioOutputs[0];
+    let length = process.framesCount;
+    for (let c = 0; c < 2; ++c) {
+      let bufferIn = audioIn.data32[c];
+      let bufferOut = audioOut.data32[c];
+      for (let i: u32 = 0; i < length; ++i) {
+        bufferOut[i] = abs(bufferIn[i]);
+      }
+    }
+    return Clap.PROCESS_CONTINUE;
+  }
+  audioPortsCount(isInput: bool): u32 {
+    return 1;
+  }
+  audioPortsGet(index: u32, isInput: bool, info: Clap.AudioPortInfo): bool {
+    if (index > 0) return false;
+;
+    info.id = 74565;
+    info.name = "main";
+    info.channelCount = 2;
+    info.portType = "stereo";
+    return true;
+  }
+}
+let pluginSpec = Clap.registerPlugin<MyPlugin>("The Pluginator", "com.example.clap.my-plugin");
+pluginSpec.vendor = "Really Cool Plugins Ltd.";
--- transform.js
+++ transform.js
@@ -1,7 +1,9 @@
-import * as assemblyscript from "assemblyscript"
-import { Transform } from "assemblyscript/transform"
+import * as assemblyscript from "assemblyscript";
+import { Transform } from "assemblyscript/transform";
+import { fileURLToPath } from "url";
 
-import * as fs from "node:fs"
+import * as fs from "node:fs";
+import * as path from "path";
 
 let Node = assemblyscript.Node;
 let NodeKind = assemblyscript.NodeKind;
@@ -10,23 +12,23 @@ let CommonFlags = assemblyscript.CommonFlags;
 let Parser = assemblyscript.Parser;
 
 function isNode(v) {
-	return typeof v?.kind == 'number';
+  return typeof v?.kind == "number";
 }
 
 function walkAst(node, callbackFn) {
-	callbackFn(node);
-	for (let key in node) {
-		let prop = node[key];
-		if (isNode(prop)) {
-			walkAst(prop, callbackFn);
-		} else if (Array.isArray(prop) && isNode(prop[0])) {
-			prop.forEach(n => walkAst(n, callbackFn));
-		}
-	}
+  callbackFn(node);
+  for (let key in node) {
+    let prop = node[key];
+    if (isNode(prop)) {
+      walkAst(prop, callbackFn);
+    } else if (Array.isArray(prop) && isNode(prop[0])) {
+      prop.forEach((n) => walkAst(n, callbackFn));
+    }
+  }
 }
 
 function getSource(node) {
-	return node.range.source.text.substring(node.range.start, node.range.end);
+  return node.range.source.text.substring(node.range.start, node.range.end);
 }
 
 /* A decorated class member like:
@@ -54,16 +56,16 @@ Assuming the following are in scope:
 	setProxyType(v: ProxyType, raw: RawType): RawType {...}
  */
 function replaceProperties(classDeclaration) {
-	let changed = false;
-	for (let i = 0; i < classDeclaration.members.length; ++i) {
-		let member = classDeclaration.members[i];
-		
-		if (member.decorators?.[0]?.name?.text == "property") {
-			let name = member.name.text;
-			let type = getSource(member.type);
-			let init = getSource(member.initializer);
-			
-			let dummyCode = `class ${classDeclaration.name.text} {
+  let changed = false;
+  for (let i = 0; i < classDeclaration.members.length; ++i) {
+    let member = classDeclaration.members[i];
+
+    if (member.decorators?.[0]?.name?.text == "property") {
+      let name = member.name.text;
+      let type = getSource(member.type);
+      let init = getSource(member.initializer);
+
+      let dummyCode = `class ${classDeclaration.name.text} {
 				@inline get ${name}() : ${type} {
 					return get${type}(${init});
 				}
@@ -71,22 +73,22 @@ function replaceProperties(classDeclaration) {
 					${init} = set${type}(v, ${init});
 				}
 			}`;
-			let parser = new Parser();
-			parser.parseFile(dummyCode, "property.ts", false);
-			let dummyClass = parser.currentSource.statements[0];
-			let dummyMembers = dummyClass.members;
+      let parser = new Parser();
+      parser.parseFile(dummyCode, "property.ts", false);
+      let dummyClass = parser.currentSource.statements[0];
+      let dummyMembers = dummyClass.members;
 
-			if (member.flags&CommonFlags.Readonly) dummyMembers.pop(); // remove setter for read-only properties
+      if (member.flags & CommonFlags.Readonly) dummyMembers.pop(); // remove setter for read-only properties
 
-			for (let m of dummyMembers) {
-				// Assign the original range to all the expressions
-				walkAst(m, n => n.range = member.range);
-			}
-			classDeclaration.members.splice(i, 1, ...dummyMembers);
-			changed = true;
-		}
-	}
-	return changed;
+      for (let m of dummyMembers) {
+        // Assign the original range to all the expressions
+        walkAst(m, (n) => (n.range = member.range));
+      }
+      classDeclaration.members.splice(i, 1, ...dummyMembers);
+      changed = true;
+    }
+  }
+  return changed;
 }
 
 /* Fixed length arrays:
@@ -99,16 +101,16 @@ becomes:
 	foo0: u8; foo1: u8, ... foo15: u8
 */
 function replaceArrays(classDeclaration) {
-	let changed = false;
-	for (let i = 0; i < classDeclaration.members.length; ++i) {
-		let member = classDeclaration.members[i];
-		
-		if (member.decorators?.[0]?.name?.text == "array") {
-			let decorator = member.decorators[0];
-			let countArg = parseInt(getSource(decorator.args?.[0]), 10);
-			let name = member.name.text;
+  let changed = false;
+  for (let i = 0; i < classDeclaration.members.length; ++i) {
+    let member = classDeclaration.members[i];
 
-			let dummyCode = `class ${classDeclaration.name.text} {
+    if (member.decorators?.[0]?.name?.text == "array") {
+      let decorator = member.decorators[0];
+      let countArg = parseInt(getSource(decorator.args?.[0]), 10);
+      let name = member.name.text;
+
+      let dummyCode = `class ${classDeclaration.name.text} {
 				@inline get ${name}() : usize {
 					return changetype<usize>(this) + offsetof<${classDeclaration.name.text}>(${JSON.stringify(name + "0")});
 				}
@@ -116,53 +118,60 @@ function replaceArrays(classDeclaration) {
 					assert(v == changetype<usize>(this) + offsetof<${classDeclaration.name.text}>(${JSON.stringify(name + "0")}), "assignment can't actually change values");
 				}
 			}`;
-			let parser = new Parser();
-			parser.parseFile(dummyCode, "array-pointer.ts", false);
-			let dummyClass = parser.currentSource.statements[0];
-			let replacements = dummyClass.members;
+      let parser = new Parser();
+      parser.parseFile(dummyCode, "array-pointer.ts", false);
+      let dummyClass = parser.currentSource.statements[0];
+      let replacements = dummyClass.members;
 
-			walkAst(replacements, n => n.range = member.range);
+      walkAst(replacements, (n) => (n.range = member.range));
 
-			for (let i = 0; i < countArg; ++i) {
-				let name = Node.createIdentifierExpression(member.name.text + i, member.name.range, member.name.quoted);
-				let field = Node.createFieldDeclaration(
-					name,
-					null/*decorators*/,
-					member.flags,
-					member.type,
-					member.initializer,
-					member.range
-				);
-				replacements.push(field);
-			}
-			
-			classDeclaration.members.splice(i, 1, ...replacements);
-			changed = true;
-		}
-	}
-	return changed;
+      for (let i = 0; i < countArg; ++i) {
+        let name = Node.createIdentifierExpression(
+          member.name.text + i,
+          member.name.range,
+          member.name.quoted,
+        );
+        let field = Node.createFieldDeclaration(
+          name,
+          null /*decorators*/,
+          member.flags,
+          member.type,
+          member.initializer,
+          member.range,
+        );
+        replacements.push(field);
+      }
+
+      classDeclaration.members.splice(i, 1, ...replacements);
+      changed = true;
+    }
+  }
+  return changed;
 }
 class WclapTransform extends Transform {
-	afterParse(parsed) {
-		console.log("Applying @property and @array transforms");
+  afterParse(parsed) {
+    console.log("Applying @property and @array transforms");
 
-		parsed.sources.forEach(source => {
-			if (/^~lib\//.test(source.normalizedPath)) return;
+    parsed.sources.forEach((source) => {
+      if (/^~lib\//.test(source.normalizedPath)) return;
 
-			let changed = true;
-			walkAst(source, node => {
-				if (node.kind == NodeKind.ClassDeclaration) {
-					if (replaceProperties(node)) changed = true;
-					if (replaceArrays(node)) changed = true;
-				}
-			});
-			if (/^example\//.test(source.normalizedPath)) return; // don't generate this one
-			
-			let transformedPath = "./transformed/" + source.normalizedPath;
-			let transformedCode = assemblyscript.ASTBuilder.build(source);
-			console.log(`	${source.normalizedPath} -> ${transformedPath}`);
-			fs.writeFileSync(transformedPath, transformedCode);
-		});
-	}
+      let changed = true;
+      walkAst(source, (node) => {
+        if (node.kind == NodeKind.ClassDeclaration) {
+          if (replaceProperties(node)) changed = true;
+          if (replaceArrays(node)) changed = true;
+        }
+      });
+      if (/^example\//.test(source.normalizedPath)) return; // don't generate this one
+      const baseDir = fileURLToPath(import.meta.url);
+      const cwd = process.cwd();
+      let transformedPath = "./transformed/" + source.normalizedPath; // will not work on windows bro. use path.join
+      if (!fs.existsSync(transformedPath))
+        fs.mkdirSync(path.dirname(transformedPath), { recursive: true });
+      let transformedCode = assemblyscript.ASTBuilder.build(source);
+      console.log(`	${source.normalizedPath} -> ${transformedPath}`);
+      fs.writeFileSync(transformedPath, transformedCode);
+    });
+  }
 }
 export default WclapTransform;
